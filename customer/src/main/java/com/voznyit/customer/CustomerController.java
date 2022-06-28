6b99999db232dd8fc61521da015d5583a0530e21
zhenekq <zhenek02ss@gmail.com>
2022-06-28 19:26:12 +0300

    Reformat code structure of project, and connect customer to docker postresql db

--- customer/src/main/java/com/voznyit/customer/CustomerController.java
+++ customer/src/main/java/com/voznyit/customer/CustomerController.java
@@ -1,6 +1,5 @@
 package com.voznyit.customer;
 
-import com.voznyit.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,9 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public record CustomerController(CustomerService customerService) {
 
     @PostMapping
-    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest){
-        log.info("New customer registration {}", customerRegistrationRequest);
-        customerService.register(customerRegistrationRequest);
+    public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
+        log.info("new customer registration {}", customerRegistrationRequest);
+        customerService.registerCustomer(customerRegistrationRequest);
     }
-
 }
